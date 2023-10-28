7a49915feb8448e71b787d1dea54178901f38f66
harshit <harshit.goel@rivigo.com>
2023-10-28 19:55:29 +0530

    ..

--- src/hooks/useUsers.js
+++ src/hooks/useUsers.js
@@ -20,7 +20,7 @@ export function useUsers(searchValue , pageNumber){
             const endPoint = `${apiEndPoints.getUserData}/${user.login}`
             return fetch(endPoint,{
               headers:{
-                'Authorization': `token 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`
+                'Authorization': `token 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`
               }
             })
           })
@@ -52,7 +52,7 @@ export function useUsers(searchValue , pageNumber){
       setError('');
       try{
         let res = await fetch(`${apiEndPoints.getUsers}?q=${searchValue}&page=${pageNumber}` , {headers:{
-          'Authorization': `token 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` // hardcoded for now due to issues with deployment
+          'Authorization': `token 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` // hardcoded for now due to issues with deployment
         }, signal : controller.signal});
         if(!res.ok){
           throw new Error("Something went wrong")
